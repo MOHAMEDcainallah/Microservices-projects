bc96ab3d0d2e882a43cd45489a2e402834c082c5
MOHAMEDcainallah <117941654+MOHAMEDcainallah@users.noreply.github.com>
2023-11-14 19:17:44 +0100

    Created The main Project Structure

---
+++ order-service/src/main/java/org/sid/orderservice/entities/Order.java
@@ -0,0 +1,35 @@
+package org.sid.orderservice.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.orderservice.enums.OrderStatus;
+import org.sid.orderservice.model.Customer;
+
+import java.util.Date;
+import java.util.List;
+@Entity
+@Table(name="orders")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Order {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date createdAt;
+    private OrderStatus status;
+    private Long customerId;
+    @Transient // cette annotation veut dire pour la jpa pour ne pas prendre en consideration ce attribut la de customer !!
+    private Customer customer;
+    @OneToMany(mappedBy ="order")
+    private List<ProductItem> productItems;
+    
+    
+    public double getTotal(){
+        double somme=0;
+        for (ProductItem pi:productItems){
+            somme+=pi.getAmount();
+        }
+        return somme;
+    }
+}
